d62bb09223fa87b16864b31cf6c171a898d04432
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-12-09 12:47:43 +0100

    add hero section (need refactor)

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,18 @@
 <template>
-	<section></section>
+	<section>
+		<article>
+			<div>
+				<h1>Complete Your Dreams in JavaS Programming</h1>
+				<p>Learn any program in distance and for a reasonable free. You don't have to struggle alone, you've got our assistance and help.</p>
+				<a href="#" class="button-primary">
+					<span><ion-icon name="download-outline"></ion-icon></span><span>Download free E-book</span>
+				</a>
+			</div>
+		</article>
+		<figure>
+			<img src="../assets/images/home-5-hero-image.png" alt="" />
+		</figure>
+	</section>
 </template>
 
 <script>
@@ -14,4 +27,59 @@ export default {
 
 <style lang="scss" scoped>
 @import "../styles/index.scss";
+
+section {
+	@extend .flex-section;
+	margin-top: 2rem;
+	padding-bottom: 1rem;
+
+	> article {
+		width: 100%;
+		display: flex;
+		padding: 1rem;
+		align-items: center;
+		color: $mulled-wine;
+		justify-content: center;
+
+		> div:first-of-type {
+			max-width: 60%;
+			> h1 {
+				font-size: 2.5rem;
+				line-height: 3rem;
+				margin-bottom: 1rem;
+			}
+
+			> .button-primary {
+				color: $white;
+				display: flex;
+				cursor: pointer;
+				margin-top: 2rem;
+				font-weight: 500;
+				padding: 1rem 2rem;
+				align-items: center;
+				border-radius: 0.5rem;
+				max-width: fit-content;
+				justify-content: center;
+				background-color: $jungle-green;
+
+				> span:first-of-type {
+					font-size: 1.5rem;
+					margin-right: 0.5rem;
+				}
+			}
+		}
+	}
+
+	> figure {
+		width: 100%;
+		display: flex;
+		padding: 1rem;
+		align-items: center;
+		justify-content: center;
+
+		> img {
+			max-width: 512px;
+		}
+	}
+}
 </style>
