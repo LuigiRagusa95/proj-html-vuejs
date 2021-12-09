4947b1f8c25fec0984e5a34b6072ba22928dcce0
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-12-09 10:52:15 +0100

    adding header and own component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+	<header>
+		<NavBar />
+		<Hero />
+	</header>
+</template>
+
+<script>
+import Hero from "./Hero.vue";
+import NavBar from "./NavBar.vue";
+export default {
+	name: "Header",
+	components: { NavBar, Hero },
+	data() {
+		return {};
+	},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+export default {
+	name: "Hero",
+	data() {
+		return {};
+	},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
