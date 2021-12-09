d398ef77539cb4ca9970f044d22bce8710930077
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-12-09 11:01:11 +0100

    import components and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<Header />
+	</div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
 	name: "App",
-	components: {},
+	components: { Header },
 };
 </script>
 
 <style lang="scss">
 @import "./styles/index.scss";
+*,
+*::before,
+*::after {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+html,
+body {
+	width: 100%;
+	height: 100%;
+	font-family: "Poppins", sans-serif;
+}
 </style>
